85a14961f695eda75f7a6818244bf33bb2eb3e4f
henrique <henriquevt98@gmail.com>
2025-03-21 22:12:21 -0300

    feat: format on save is kill

--- init.lua
+++ init.lua
@@ -649,29 +649,18 @@ require('lazy').setup {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
+      format_on_save = false,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
       },
     },
   },
@@ -1009,6 +998,20 @@ require('lazy').setup {
   --   },
   -- },
   -- misc utils
+  {
+    'ruifm/gitlinker.nvim',
+    opts = {},
+    keys = {
+      {
+        '<leader>gy',
+        mode = { 'n', 'v' },
+        function()
+          require('gitlinker').get_buf_range_url 'n'
+        end,
+        desc = 'Copy remote URL for selection',
+      },
+    },
+  },
   { 'sindrets/diffview.nvim', opts = {} },
   {
     'akinsho/bufferline.nvim',
