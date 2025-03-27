8501b6aa9f8afa323be9f94efc0c697c4b55f767
henrique <henriquevt98@gmail.com>
2025-03-27 19:40:52 -0300

    fix: feeble attempts to add graphql

--- init.lua
+++ init.lua
@@ -572,6 +572,7 @@ require('lazy').setup {
         gopls = {},
         pyright = {},
         rust_analyzer = {},
+        graphql = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -844,7 +845,7 @@ require('lazy').setup {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'graphql' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
